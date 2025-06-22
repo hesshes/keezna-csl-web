777d5c6f882772f16c178e56fe0b97884d4c868f
taewoo kim <hesshes@naver.com>
2025-06-22 20:45:53 +0900

    2025-06-22

--- src/main/java/com/csl/keezna/web/config/CustomServletConfig.java
+++ src/main/java/com/csl/keezna/web/config/CustomServletConfig.java
@@ -2,7 +2,6 @@ package com.csl.keezna.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,18 +16,8 @@ public class CustomServletConfig implements WebMvcConfigurer {
 	}
 
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*")
-				.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS").maxAge(300)
-				.allowedHeaders("Authorization", "Cache-Control", "Content-Type");
-	}
-
-	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
 		WebMvcConfigurer.super.addResourceHandlers(registry);
-
 		registry.addResourceHandler("/api/products/images/**").addResourceLocations("file:/Temp/upload/");
-
 	}
 }
